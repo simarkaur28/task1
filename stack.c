93948dad913d73c364be3ff9edef546bd5a61b70
SWAROOPBHAGWAT <swaroopbhagwat369@gmail.com>
2022-08-05 16:53:46 +0530

    changes

--- stack.c
+++ stack.c
@@ -80,17 +80,7 @@ void show()
 			printf("%d\n",inp_array[i]);
 	}
 }
-void show()
+void main()
 {
-	
-	
-	if(Top==-1)
-	{
-		printf("\nUnderflow!!");
-	}
-	else
-	{
-		printf("\nElements present in the stack: \n");
-		for(int i=Top;i>=0;--i)
-			printf("%d\n",inp_array[i]);
-	}
+	//Hello 
+}
