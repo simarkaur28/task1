ae69ab7724f6af8dc1f0b7c070a651d8c3167d76
yuvrajrathod123 <110601973+yuvrajrathod123@users.noreply.github.com>
2022-08-15 00:31:35 +0530

    Update stack.c
    Changed the size of stack and added a print statement.

--- stack.c
+++ stack.c
@@ -2,7 +2,7 @@
 
 #include<stdlib.h>
  
-#define Size 6 
+#define Size 9 
  
 int Top=-1, inp_array[Size];
 void Push();
@@ -83,5 +83,5 @@ void show()
 
 void main ()
 {
-	new code	
+	printf("stack program");
 }
